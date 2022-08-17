e66ddb834e0df02ab60a7d0eaf68de411e566e3c
martinfs1 <martinfernando-sandander@hotmail.com>
2022-08-17 12:35:12 -0300

    01, 02 exercises finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {    
 
+    let rString = "";
+
+    if (num>=0) {
+        for (let i = 0; i < num; i++) {
+            rString += string               
+        }     
+    } else {
+        return 'ERROR'
+    }
+
+    return rString  
 };
 
 // Do not edit below this line
