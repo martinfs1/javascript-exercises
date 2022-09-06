fd704ccff5d310b65c74d025c994babfce84dff1
martinfs1 <martinfernando-sandander@hotmail.com>
2022-09-06 11:39:53 -0300

    all task of exercise 05 are solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (numStart, numEnd) {
+  let sum = 0; 
 
+  if ( (numStart < 0 || numEnd < 0) || (!Number.isInteger(numStart) || !Number.isInteger(numEnd)) ) {
+    return "ERROR";
+  } else if (numStart > numEnd) {
+    for (let index = numEnd; index <= numStart; index++) {
+      sum += index;
+    }
+    return sum;
+  } else {
+    for (let index = numStart; index <= numEnd; index++) {
+      sum += index;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
