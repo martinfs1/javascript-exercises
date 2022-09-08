289a9d56b5600be6689bcb23b9a14c885892106d
martinfs1 <martinfernando-sandander@hotmail.com>
2022-09-08 10:15:52 -0300

    all task on exercise 07 are solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,12 @@
 const sumAll = function (numStart, numEnd) {
-  let sum = 0; 
+  let sum = 0;
 
-  if ( (numStart < 0 || numEnd < 0) || (!Number.isInteger(numStart) || !Number.isInteger(numEnd)) ) {
+  if (
+    numStart < 0 ||
+    numEnd < 0 ||
+    !Number.isInteger(numStart) ||
+    !Number.isInteger(numEnd)
+  ) {
     return "ERROR";
   } else if (numStart > numEnd) {
     for (let index = numEnd; index <= numStart; index++) {
@@ -18,4 +23,3 @@ const sumAll = function (numStart, numEnd) {
 
 // Do not edit below this line
 module.exports = sumAll;
-
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+const ftoc = function(fValue) {
+  let celsius = 0;
+  if (!isNaN(fValue)) {
+    celsius = Number.parseFloat((fValue-32) * 5/9).toFixed(1);
+    return Number(celsius)
+  }
 
 };
 
-const ctof = function() {
+const ctof = function(cValue) {
+  let fahrenheit= 0;
+  if (!isNaN(cValue)) {
+    fahrenheit = Number.parseFloat(cValue*9/5+32).toFixed(1);
+    return Number(fahrenheit)
+  }
 
 };
 
