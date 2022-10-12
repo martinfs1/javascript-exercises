356a9230e32f6b4da4a91ff6c83c0149fb8a53ed
martinfs1 <martinfernando-sandander@hotmail.com>
2022-10-12 13:04:56 -0300

    calculator finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let numA = 0;
+  arr.forEach((item) => (numA += item));
+  return numA;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let result = arr.reduce((total, currentValue) => total * currentValue);
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let result = Math.pow(num1, num2);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = num;
+  if (num === 0 || num === 1) {
+    return result = 1;
+  } else {
+    for (let i = num; i > 1; i--) {
+      result *= (i - 1); 
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
