a88e83abb70ac80daaeacde90ba0cfc46288e6df
martinfs1 <martinfernando-sandander@hotmail.com>
2022-10-25 12:20:21 -0300

    I've implement string metods to solve these tasks.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  
+  let testedStr = str.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "").toLowerCase();  
 
+  let reverseStr = "";
+  for (let i = testedStr.length - 1; i >= 0; i--) {
+    reverseStr += testedStr[i];
+  }
+  return testedStr === reverseStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
