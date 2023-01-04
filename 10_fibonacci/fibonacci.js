3886e20ad9c441b6722badca72e8b59a575280b2
martinfs1 <martinfernando-sandander@hotmail.com>
2023-01-04 12:11:19 -0300

    fibonacci no solved by me (recursive)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+let stringToNumber = Number(member)
+  if (stringToNumber < 0) {
+    return `OOPS`;
+  } else if (stringToNumber < 2) {
+    return stringToNumber;
+  } else {
+    return (fibonacci(stringToNumber-1) + fibonacci(stringToNumber-2))
+  }
+  
 };
 
 // Do not edit below this line
