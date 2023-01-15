778b8f1fbe25b7446d798f2fe1e8fc6a0d1cbae4
martinfs1 <martinfernando-sandander@hotmail.com>
2023-01-15 13:50:34 -0300

    tests solved by me!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let yearNow = new Date().getFullYear();
+  let personAlive;
+  
+  let arrayOfYearsOld = people.map((person) => {
+    return (!Object.hasOwn(person, "yearOfDeath"))
+      ? yearNow - person.yearOfBirth
+      : person.yearOfDeath - person.yearOfBirth;
+  });
+  
+  let yearsOld = Math.max(...arrayOfYearsOld);
 
+  for (let i = 0; i < people.length; i++) {
+    const element = people[i];
+    if (!Object.hasOwn(element, "yearOfDeath")) {
+      personAlive = yearNow - element.yearOfBirth
+      if (personAlive === yearsOld) {
+        return element;
+      }
+    }else if (element.yearOfDeath - element.yearOfBirth === yearsOld) {
+      return element;    
+    }       
+  }
 };
 
 // Do not edit below this line
